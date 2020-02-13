253c707729a51894d9008fcddfd9a8b5354f4136
Sascha Adler <sadler@crkn.ca>
2020-02-13 11:53:13 -0700

    Default HAProxy config. Uses IP addresses to be statically defined in each access platform repo

---
+++ haproxy/generate_test_cert.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+DIR=`dirname "$0"`
+
+openssl req -x509 \
+  -newkey rsa:4096 \
+  -sha256 \
+  -days 3650 \
+  -nodes \
+  -keyout $DIR/localhost.key \
+  -out $DIR/localhost.crt \
+  -subj /CN=localhost \
+  -addext subjectAltName=DNS:*.canadiana.ca
+
+cat $DIR/localhost.crt $DIR/localhost.key > $DIR/localhost.pem
