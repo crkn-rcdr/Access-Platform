24a1c4892084c11e08de4a883166ec4e56069d2c
Russell McOrmond <rmcormond@crkn.ca>
2020-04-21 13:25:22 -0400

    Simple view to look up a slug or slug alias.

---
+++ Databases/design-docs/slug/design/access/views/aliases.js
@@ -0,0 +1,11 @@
+module.exports = {
+    // Map includes aliases and document ID's for single lookup.
+    map: function(doc) {
+      if ('aliases' in doc && Array.isArray(doc.aliases)) {
+        doc.aliases.forEach(function(thisalias) {
+          emit(thisalias, null);
+        });
+      };
+      emit(doc['_id'],null);
+    }
+  };
