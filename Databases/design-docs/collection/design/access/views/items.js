b726823da132e6388115be83046f262c9bb44651
Sascha Adler <sadler@crkn.ca>
2020-05-26 18:29:01 -0600

    Updates collection/_design/access/_view/items to emit the parent collection's label, in order to more easily identify what the parent collection is

--- Databases/design-docs/collection/design/access/views/items.js
+++ Databases/design-docs/collection/design/access/views/items.js
@@ -1,9 +1,10 @@
 module.exports = {
-  // Map includes aliases and document ID's for single lookup.
+  // allows for the lookup of which collections a particular item is in.
+  // emits the document label for ease of identifying the collection
   map: function (doc) {
     if ("items" in doc && Array.isArray(doc.items)) {
       doc.items.forEach(function (item) {
-        emit(item, null);
+        emit(item, doc.label);
       });
     }
   },
