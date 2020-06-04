c12cd1fdc67e529871414eb4ccb85fa882950482
Russell McOrmond <rmcormond@crkn.ca>
2020-06-04 18:01:07 -0400

    Prettier formatting, and split date into components for key (sorts the same as string for smelter queue, but can be used for more interesting displaying.

--- Databases/design-docs/dipstaging/design/sync/views/smeltq.js
+++ Databases/design-docs/dipstaging/design/sync/views/smeltq.js
@@ -1,13 +1,19 @@
 module.exports = {
   map: function (doc) {
-    if ("smelt" in doc && "requestDate" in doc["smelt"]) {
+    if ("smelt" in doc && "requestDate" in doc.smelt) {
       if (
-        "processDate" in doc["smelt"] &&
-        doc["smelt"]["processDate"] >= doc["smelt"]["requestDate"]
+        !("processDate" in doc.smelt) ||
+        doc.smelt.processDate < doc.smelt.requestDate
       ) {
-        return;
+        // Seems that Date.parse doesn't support this RFC 3339 date format, so using regexp
+        var mandateParse = /^(\d{4})-(\d{2})-(\d{2})T(\d{2}):(\d{2}):(\d{2})Z$/;
+        var parsedate = mandateParse.exec(doc.smelt.requestDate);
+        if (parsedate) {
+          // first element is the string again, so get rid of it and emit array with each matched element
+          parsedate.shift();
+          emit(parsedate, null);
+        }
       }
-      emit(doc["smelt"]["requestDate"], null);
     }
   },
   reduce: "_count",
