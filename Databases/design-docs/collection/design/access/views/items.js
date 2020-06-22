b2e99ccb5b41370b6916cf9e81127bafbb14fa13
Sascha Adler <sadler@crkn.ca>
2020-06-22 16:08:21 -0600

    Updates the items map to also emit the parent collection slug

--- Databases/design-docs/collection/design/access/views/items.js
+++ Databases/design-docs/collection/design/access/views/items.js
@@ -1,10 +1,10 @@
 module.exports = {
   // allows for the lookup of which collections a particular item is in.
-  // emits the document label for ease of identifying the collection
+  // emits the document slug and label for ease of identifying the collection
   map: function (doc) {
     if ("items" in doc && Array.isArray(doc.items)) {
       doc.items.forEach(function (item) {
-        emit(item, doc.label);
+        emit(item, { slug: doc.slug, label: doc.label });
       });
     }
   },
