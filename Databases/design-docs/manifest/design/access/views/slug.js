25f43183d1b8560077ded1f26553dbf0e630973e
Sascha Adler <sadler@crkn.ca>
2020-06-22 14:46:34 -0600

    Updates slug view maps to also emit the slug aliased slugs are aliases of

--- Databases/design-docs/manifest/design/access/views/slug.js
+++ Databases/design-docs/manifest/design/access/views/slug.js
@@ -3,11 +3,12 @@ module.exports = {
   map: function (doc) {
     if ("slug" in doc) {
       emit(doc.slug, { label: doc.label, isAlias: false });
-    }
-    if ("aliasedSlugs" in doc && Array.isArray(doc.aliasedSlugs)) {
-      doc.aliasedSlugs.forEach(function (slug) {
-        emit(slug, { label: doc.label, isAlias: true });
-      });
+
+      if ("aliasedSlugs" in doc && Array.isArray(doc.aliasedSlugs)) {
+        doc.aliasedSlugs.forEach(function (slug) {
+          emit(slug, { label: doc.label, isAlias: true, aliasOf: doc.slug });
+        });
+      }
     }
   },
 };
