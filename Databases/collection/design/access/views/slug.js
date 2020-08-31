18c04144ca1baab3170918b111b00253e37de748
Russell McOrmond <rmcormond@crkn.ca>
2020-08-31 07:40:46 -0400

    Add boolean to view for 'public'.   Prettier code formatter.

--- Databases/collection/design/access/views/slug.js
+++ Databases/collection/design/access/views/slug.js
@@ -2,11 +2,20 @@ module.exports = {
   // emit for slugs and aliases
   map: function (doc) {
     if ("slug" in doc) {
-      emit(doc.slug, { label: doc.label, isAlias: false });
+      emit(doc.slug, {
+        label: doc.label,
+        isAlias: false,
+        public: "public" in doc,
+      });
 
       if ("aliasedSlugs" in doc && Array.isArray(doc.aliasedSlugs)) {
         doc.aliasedSlugs.forEach(function (slug) {
-          emit(slug, { label: doc.label, isAlias: true, aliasOf: doc.slug });
+          emit(slug, {
+            label: doc.label,
+            isAlias: true,
+            aliasOf: doc.slug,
+            public: "public" in doc,
+          });
         });
       }
     }
