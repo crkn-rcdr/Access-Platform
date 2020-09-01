07e5d7852822be1ceaeb7d68bdfa4d7f86348f2a
Russell McOrmond <rmcormond@crkn.ca>
2020-09-01 08:58:10 -0400

    View to find when reposManifestDate is newer than the smelt.processDate.   View to find IDs where no smelt was requested.

---
+++ Databases/dipstaging/design/sync/views/newerrepo.js
@@ -0,0 +1,20 @@
+module.exports = {
+  map: function (doc) {
+    if (
+      "reposManifestDate" in doc &&
+      "smelt" in doc &&
+      "processDate" in doc.smelt &&
+      doc.reposManifestDate > doc.smelt.processDate
+    ) {
+      // Seems that Date.parse doesn't support this RFC 3339 date format, so using regexp
+      var mandateParse = /^(\d{4})-(\d{2})-(\d{2})T(\d{2}):(\d{2}):(\d{2})Z$/;
+      var parsedate = mandateParse.exec(doc["reposManifestDate"]);
+      if (parsedate) {
+        // first element is the string again, so get rid of it and emit array with each matched element
+        parsedate.shift();
+        emit(parsedate, null);
+      }
+    }
+  },
+  reduce: "_count",
+};
---
+++ Databases/dipstaging/design/sync/views/nosmelt.js
@@ -0,0 +1,15 @@
+module.exports = {
+  map: function (doc) {
+    if ("reposManifestDate" in doc && !("smelt" in doc)) {
+      // Seems that Date.parse doesn't support this RFC 3339 date format, so using regexp
+      var mandateParse = /^(\d{4})-(\d{2})-(\d{2})T(\d{2}):(\d{2}):(\d{2})Z$/;
+      var parsedate = mandateParse.exec(doc["reposManifestDate"]);
+      if (parsedate) {
+        // first element is the string again, so get rid of it and emit array with each matched element
+        parsedate.shift();
+        emit(parsedate, null);
+      }
+    }
+  },
+  reduce: "_count",
+};
