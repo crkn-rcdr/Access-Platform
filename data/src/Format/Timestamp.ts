4622004109b9e62b5bf86989f7fd70297ff8a93b
Sascha Adler <sadler@crkn.ca>
2021-03-10 20:38:19 -0330

    Updates the timestamp format to suit our specific needs a bit more

--- data/src/Format/Timestamp.ts
+++ data/src/Format/Timestamp.ts
@@ -1,14 +1,12 @@
 import { generateFormat } from "../validator";
 
-/**
- * This is `date-time`, according to the JSON Schema spec.
- */
 const name = "timestamp";
-// See https://github.com/ajv-validator/ajv-formats/blob/master/src/formats.ts
-const regex = /^\d\d\d\d-[0-1]\d-[0-3]\d[t\s](?:[0-2]\d:[0-5]\d:[0-5]\d|23:59:60)(?:\.\d+)?(?:z|[+-]\d\d(?::?\d\d)?)$/i;
+const regex = /^\d{4}-(?:0\d|1[0-2])-(?:[0-2]\d|3[0-1])t[0-2]\d:[0-5]\d:(?:[0-5]\d|60)(?:\.\d+)?z$/i;
 
 /**
- * An ISO 8601 timestamp.
+ * An ISO 6801 timestamp akin to JSON Schema's `date-time`, but only accepts
+ * times in UTC. Note that decimal places after the number of seconds are
+ * allowed.
  */
 export type Timestamp = string;
 
