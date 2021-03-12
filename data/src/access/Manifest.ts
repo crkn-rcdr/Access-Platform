814573139963fb6eee3f0c34d5112a6f0e4b7e0f
Sascha Adler <sadler@crkn.ca>
2021-03-12 11:34:10 -0330

    Corrects the spelling of `continuous`

--- data/src/access/Manifest.ts
+++ data/src/access/Manifest.ts
@@ -2,7 +2,7 @@ import { JSONSchemaType } from "ajv";
 import { inherit } from "../validator";
 import { Canonical, schema as canonicalSchema } from "./Canonical";
 
-const BEHAVIORS = ["unordered", "individuals", "contiuous", "paged"];
+const BEHAVIORS = ["unordered", "individuals", "continuous", "paged"];
 const DIRECTIONS = [
   "left-to-right",
   "right-to-left",
@@ -15,11 +15,13 @@ type ManifestSpec = {
 
   /**
    * Semantics about what the order of the series of images means.
+   * Default value: `continuous`
    */
   behavior?: typeof BEHAVIORS[number];
 
   /**
    * Direction the manifest's images are meant to be viewed in.
+   * Default value: `left-to-right`
    */
   viewingDirection?: typeof DIRECTIONS[number];
 };
