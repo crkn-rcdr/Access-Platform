ffb92435aa58ec48c2ded499d34efbde538c8ee2
Sascha Adler <sadler@crkn.ca>
2021-03-19 18:52:38 -0230

    Timestamp can either be a number or a string

--- data/src/format/index.ts
+++ data/src/format/index.ts
@@ -1,4 +1,3 @@
 export * as Noid from "./noid";
 export * as Slug from "./slug";
-export * as Timestamp from "./timestamp";
 export * as UnixFilePath from "./unixFilePath";
--- data/src/util/index.ts
+++ data/src/util/index.ts
@@ -1,4 +1,5 @@
 export * as FileRef from "./FileRef";
 export * as ImageRef from "./ImageRef";
 export * as ProcessUpdate from "./ProcessUpdate";
+export * as Timestamp from "./Timestamp";
 export * as Text from "./Text";
