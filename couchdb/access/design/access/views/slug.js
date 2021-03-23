58bd9148e79f653826782d8acbf94674cc9cad88
Russell McOrmond <rmcormond@crkn.ca>
2021-03-23 09:59:14 -0400

    Emit null -- getting out of memory errors again...

--- couchdb/access/design/access/views/slug.js
+++ couchdb/access/design/access/views/slug.js
@@ -2,10 +2,7 @@ module.exports = {
   // emit for slugs and aliases
   map: function (doc) {
     if ("slug" in doc) {
-      emit(doc.slug, {
-        label: doc.label,
-        public: "public" in doc,
-      });
+      emit(doc.slug, null);
     }
   },
 };
