3956704f7ffc09e77ce0b40186fc687bbeaad156
Sascha Adler <sadler@crkn.ca>
2021-03-31 19:18:12 -0230

    Slight simplification

--- couchdb/access/design/noid/views/canvasnoids.js
+++ couchdb/access/design/noid/views/canvasnoids.js
@@ -1,11 +1,9 @@
 module.exports = {
   map: function (doc) {
-    if ("canvases" in doc && Array.isArray(doc.canvases)) {
-      doc.canvases.forEach(function (thiscanvas) {
-        if ("id" in thiscanvas) {
-          emit(thiscanvas.id, null);
-        }
-      });
+    if (Array.isArray(doc.canvases)) {
+      for (const canvas of doc.canvases) {
+        emit(canvas.id, null);
+      }
     }
   },
 };
