3eaa03a705dd3d5aa8d543731f5c030bd2b2aee5
Brittny Lapierre <brittny.lapierre@gmail.com>
2021-05-19 15:34:54 -0400

    dipstaging files generating

--- packages/data/src/couch/index.ts
+++ packages/data/src/couch/index.ts
@@ -1,13 +1,16 @@
 import { AccessDocument } from "./access";
 import { CanvasDocument } from "./canvas";
+import { DipstagingDocument } from "./dipstaging";
 
 export type DocumentTypes = {
   access: AccessDocument;
   canvas: CanvasDocument;
+  dipstaging: DipstagingDocument;
 };
 
-export const DATABASES = ["access", "canvas"] as const;
+export const DATABASES = ["access", "canvas", "dipstaging"] as const;
 
 export { validate as validateAccessDocument, toAccessObject } from "./access";
 export { validate as validateCanvasDocument } from "./canvas";
+export { validate as validateDipstagingDocument } from "./dipstaging";
 export { fromCouch, toCouch } from "./util";
