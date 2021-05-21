a7b86c3741201bfca4964c1ead3f72634265d39a
Brittny Lapierre <brittny.lapierre@gmail.com>
2021-05-21 15:38:34 -0400

    dipstaging / staff update (#139)

---
+++ packages/data/src/couch/dipstaging.ts
@@ -0,0 +1,15 @@
+import { Document as CouchDocument, toCouchSchema } from "./util";
+import { Dipstaging, schema as dipstagingSchema } from "../dipstaging";
+import { generateSchema } from "../validator";
+
+/**
+ * A document in the `dipstaging` Couch database.
+ */
+export type DipstagingDocument = CouchDocument<Dipstaging>;
+
+
+export const { inline, schema, validate } = generateSchema<DipstagingDocument>({
+  ...toCouchSchema(dipstagingSchema),
+  $id: "/couch/dipstaging",
+  description: "A document in the `dipstaging` Couch database.",
+});
