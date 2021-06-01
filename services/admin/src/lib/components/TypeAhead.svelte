e4995fafee02d9b4400b02249540afc74ba80ad8
DivyaKrishnan26 <dkrishnan@crkn.ca>
2021-06-01 13:29:30 -0400

    Slug search a prototype

---
+++ services/admin/src/lib/components/TypeAhead.svelte
@@ -0,0 +1,65 @@
+<script>
+  import { createEventDispatcher } from "svelte";
+
+  const dispatch = createEventDispatcher();
+
+  export let label = "Please provide a label for this component.";
+
+  let prefix = "";
+  let lookupList = [];
+  let error = "";
+
+  async function lookupSlug() {
+    if (prefix) {
+      let response = await fetch(`/slug/search/${prefix}.json`, {
+        method: "POST",
+        credentials: "same-origin",
+      });
+      console.log("lookup response", response);
+      let json = await response.json();
+      if (response.status === 200) {
+        lookupList = json;
+        console.log("what in lookup", lookupList);
+      } else {
+        error = json.error;
+      }
+    }
+  }
+
+  async function selectItem(event) {
+    console.log("***lookuplist***", lookupList);
+    if (lookupList && lookupList.includes(prefix)) {
+      let response = await fetch(`/slug/${prefix}.json`, {
+        credentials: "same-origin",
+      });
+      let slug = await response.json();
+      if (response.status === 200) {
+        dispatch("selected", slug);
+      } else {
+        error = slug.error;
+      }
+    }
+  }
+</script>
+
+<label for="slugInput">{label}</label>
+<input
+  type="text"
+  id="slugInput"
+  list="slugList"
+  bind:value={prefix}
+  on:input={lookupSlug}
+  on:change={selectItem}
+/>
+
+<datalist id="slugList">
+  {#if lookupList}
+    {#each lookupList as item}
+      <option>{item}</option>
+    {/each}
+  {/if}
+</datalist>
+
+{#if error}
+  <p class="danger">{error}</p>
+{/if}
--- services/admin/src/routes/index.svelte
+++ services/admin/src/routes/index.svelte
@@ -1,5 +1,6 @@
 <script>
-  import Resolver from "../components/Resolver.svelte";
+  import Resolver from "$lib/components/Resolver.svelte";
+  import TypeAhead from "$lib/components/TypeAhead.svelte";
 </script>
 
 <h1>Welcome to SvelteKit</h1>
@@ -7,4 +8,5 @@
   Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
 </p>
 
-<Resolver slug="" noid="" />
+<Resolver slug="oocihm.00001" noid="69429/m0w950g3k03r" />
+<TypeAhead label="Type Slug List:" />
