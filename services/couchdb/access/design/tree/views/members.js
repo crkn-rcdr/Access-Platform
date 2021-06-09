49cb8424d7a90357544c11c058ce7c32f7c09bd9
Sascha Adler <sadler@crkn.ca>
2021-06-09 20:33:22 -0230

    Creates a prelude for design docs, and uses it where helpful. Adds tests for design doc functions that use the prelude

--- services/couchdb/access/design/tree/views/members.js
+++ services/couchdb/access/design/tree/views/members.js
@@ -1,7 +1,7 @@
 module.exports = {
   map: function (doc) {
     if (Array.isArray(doc.members) && doc.members.length > 0)
-      emit([doc.behavior, doc.members.length ], doc.slug );
+      emit([doc.behavior, doc.members.length], doc.slug);
   },
   reduce: "_count",
 };
