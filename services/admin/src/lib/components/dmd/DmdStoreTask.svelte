e244a17a7e244d66a3c5433dc8ecd40146a247a6
Brittny Lapierre <brittny.lapierre@gmail.com>
2021-08-10 16:30:12 -0400

    Done documenting DmdStoreTask

--- services/admin/src/lib/components/dmd/DmdStoreTask.svelte
+++ services/admin/src/lib/components/dmd/DmdStoreTask.svelte
@@ -1,12 +1,14 @@
 <!--
 @component
 ### Overview
+This component shows the user the progress of and result for each store request (adding the metadata files to the access and/or preservation databases) for the items generated in the split task.
 
 ### Properties
 none
 
 ### Usage
 ```  
+<DmdStoreTask />
 ```
 *Note: `bind:` is required for changes to the parameters to be reflected in higher level components.*
 -->
