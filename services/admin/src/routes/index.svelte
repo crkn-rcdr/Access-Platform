6173a11e44cf8e3c70f9dba09cd4a9bf60512bc4
Brittny Lapierre <brittny.lapierre@gmail.com>
2021-08-11 13:53:20 -0400

    Add Noid.parse check on search results table in home page

--- services/admin/src/routes/index.svelte
+++ services/admin/src/routes/index.svelte
@@ -5,6 +5,7 @@
    */
   import { goto } from "$app/navigation";
   import TypeAhead from "$lib/components/access-objects/TypeAhead.svelte";
+  import { Noid } from "@crkn-rcdr/access-data";
 
   /**
    * Routes to the object the user clicks from the TypeAhead component
@@ -13,7 +14,11 @@
    */
   function slugSelected(event: CustomEvent<string>) {
     const noid = event.detail;
-    if (noid) goto(`/object/${noid}`);
+    try {
+      if (Noid.parse(noid)) goto(`/object/${noid}`);
+    } catch (e) {
+      console.log(e);
+    }
   }
 </script>
 
