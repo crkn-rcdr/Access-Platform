83ceef880913d067be9e0037109059a876e5bdfb
Brittny Lapierre <brittny.lapierre@gmail.com>
2021-08-12 12:25:58 -0400

    Waiting layout with loader and important fields

--- services/admin/src/routes/dmd/[taskId]/index.svelte
+++ services/admin/src/routes/dmd/[taskId]/index.svelte
@@ -1,36 +1,16 @@
-<script context="module" lang="ts">
-  /**
-   * @module
-   * @description loads in the dmdtask from the backend using the params in the route of the page
-   */
-</script>
-
 <script lang="ts">
-  /**
-   * @file
-   * @description This page displays the various states of and information about a dmdtask
-   */
   import { getStores } from "$app/stores";
   import type { Session } from "$lib/types";
-  import type {
-    WaitingDMDTask,
-    FailedDMDTask,
-    SucceededDMDTask,
-  } from "@crkn-rcdr/access-data";
-  import DmdSplitWaitingViewer from "$lib/components/dmd/DmdSplitWaitingViewer.svelte";
-  import DmdSplitFailureViewer from "$lib/components/dmd/DmdSplitFailureViewer.svelte";
-  import DmdSplitSuccessStoreForm from "$lib/components/dmd/DmdSplitSuccessStoreForm.svelte";
+  import Loading from "$lib/components/shared/Loading.svelte";
+  import type { WaitingDMDTask } from "@crkn-rcdr/access-data";
 
   /**
    * @type {Session} The session store that contains the module for sending requests to lapin.
    */
   const { session } = getStores<Session>();
 
-  /* TODO: Later grab from backend. */
-  /**
-   * @type {WaitingDMDTask | FailedDMDTask | SucceededDMDTask} The dmdtask being displayed by the page.
-   */
-  const dmdTask: WaitingDMDTask | FailedDMDTask | SucceededDMDTask = {
+  /* Later grab from backend. */
+  const dmdTask: WaitingDMDTask = {
     id: "123",
     updated: "1628785112",
     attachments: {
@@ -40,38 +20,44 @@
     mdType: "marcooe",
     process: {
       requestDate: "1628785101",
-      //succeeded: false,
-      //message: "The process.message from the backend",
     },
-    //items: [{}],
   };
 </script>
 
-<div class="dmd-task-page-wrap">
-  {#if !("succeeded" in dmdTask?.["process"])}
-    <DmdSplitWaitingViewer {dmdTask} />
-  {:else if !dmdTask?.["process"]?.["succeeded"]}
-    <DmdSplitFailureViewer
-      {dmdTask}
-      message={dmdTask?.["process"]?.["message"]}
-    />
-  {:else if dmdTask?.["items"]?.length}
-    <DmdSplitSuccessStoreForm />
-  {:else}
-    <!--JUUUST In Case-->
-    <DmdSplitFailureViewer
-      {dmdTask}
-      message="No objects were split from the metadata file."
-    />
-  {/if}
+<div class="hero hero__gradient full-page">
+  <div class="wrapper">
+    <br />
+    <br />
+    <br />
+    <div class="auto-align auto-align__block auto-align__j-center">
+      <Loading />
+    </div>
+    <br />
+    <div
+      class="auto-align auto-align__block auto-align__column auto-align__a-center"
+    >
+      <h6>Please wait while your DMD Task is processing...</h6>
+      <div class="dmd-request-info">
+        <span>Request inititated:</span>
+        <span>{dmdTask?.["updated"]}</span>
+        <span>Request updated:</span>
+        <span>{dmdTask?.["process"]?.["requestDate"]}</span>
+      </div>
+    </div>
+  </div>
 </div>
 
 <style>
-  :global(.dmd-task-page-wrap h6) {
+  h6 {
     text-align: center;
-    font-weight: bold;
   }
-  :global(.dmd-task-page-wrap .notification-bar) {
-    width: 30rem;
+  .dmd-request-info {
+    width: fit-content;
+    display: inline-grid;
+    display: grid;
+    grid-template-areas: "a a";
+    gap: 2rem;
+    align-items: center;
+    margin-top: var(--perfect-fourth-4);
   }
 </style>
