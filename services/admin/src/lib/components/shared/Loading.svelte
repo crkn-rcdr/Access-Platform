e3fb4e97dfba2cd577e885d8522fc64ddf1bc9d0
Brittny Lapierre <brittny.lapierre@gmail.com>
2021-08-12 12:25:58 -0400

    Waiting layout with loader and important fields

---
+++ services/admin/src/lib/components/shared/Loading.svelte
@@ -0,0 +1,88 @@
+<!--
+@component
+### Overview
+This component shows a CRKN-esque loader to be used when an action is being performed.
+
+### Properties
+|    |    |    |
+| -- | -- | -- |
+| backgroundType: "light" or "dark" or "primary" or "secondary" or "gradient"    | optional | Sets the background colour of the lines in the loader. |
+
+### Usage
+```  
+<Loader/>
+```
+-->
+<script lang="ts">
+  /**
+   * @type {"light" | "dark" | "primary" | "secondary" | "gradient"} If the file selector should support selecting multiple files or not.
+   */
+  export let backgroundType:
+    | "light"
+    | "dark"
+    | "primary"
+    | "secondary"
+    | "gradient" = "light";
+</script>
+
+<div class="loader auto-align auto-align__a-center">
+  <div class={`line line__${backgroundType}`} />
+  <div class={`line line__${backgroundType}`} />
+  <div class={`line line__${backgroundType}`} />
+</div>
+
+<style>
+  .loader {
+    height: 4rem;
+    width: fit-content;
+  }
+  .loader:last-child {
+    margin-right: 0 !important;
+  }
+  .loader .line:nth-last-child(1) {
+    animation: growShrink 2s 1s infinite;
+  }
+  .loader .line:nth-last-child(2) {
+    animation: growShrink 2s 0.5s infinite;
+  }
+  .loader .line:nth-last-child(3) {
+    animation: growShrink 2s 0s infinite;
+  }
+  .line {
+    display: inline-block;
+    width: 1rem;
+    height: 1rem;
+    border-radius: 1rem;
+    margin-right: var(--margin-sm);
+  }
+  .line__primary {
+    background: var(--primary);
+  }
+  .line__secondary {
+    background: var(--secondary);
+  }
+  .line__light {
+    background: var(--light-font);
+  }
+  .line__dark {
+    background: var(--dark-font);
+  }
+  .line__gradient {
+    background: linear-gradient(
+      40.79deg,
+      var(--teal) -106.9%,
+      var(--green) 93.69%
+    );
+  }
+  @keyframes growShrink {
+    0% {
+      height: 1rem;
+    }
+    50% {
+      height: 3rem;
+    }
+    100% {
+      height: 1rem;
+    }
+  }
+</style>
--- services/admin/src/routes/dmd/[taskId]/index.svelte
+++ services/admin/src/routes/dmd/[taskId]/index.svelte
@@ -1,1 +1,63 @@
-task specific page ...waiting
+<script lang="ts">
+  import { getStores } from "$app/stores";
+  import type { Session } from "$lib/types";
+  import Loading from "$lib/components/shared/Loading.svelte";
+  import type { WaitingDMDTask } from "@crkn-rcdr/access-data";
+
+  /**
+   * @type {Session} The session store that contains the module for sending requests to lapin.
+   */
+  const { session } = getStores<Session>();
+
+  /* Later grab from backend. */
+  const dmdTask: WaitingDMDTask = {
+    id: "123",
+    updated: "1628785112",
+    attachments: {
+      // ...metadata file info
+    },
+    user: $session.user, //for now
+    mdType: "marcooe",
+    process: {
+      requestDate: "1628785101",
+    },
+  };
+</script>
+
+<div class="hero hero__gradient full-page">
+  <div class="wrapper">
+    <br />
+    <br />
+    <br />
+    <div class="auto-align auto-align__block auto-align__j-center">
+      <Loading />
+    </div>
+    <br />
+    <div
+      class="auto-align auto-align__block auto-align__column auto-align__a-center"
+    >
+      <h6>Please wait while your DMD Task is processing...</h6>
+      <div class="dmd-request-info">
+        <span>Request inititated:</span>
+        <span>{dmdTask?.["updated"]}</span>
+        <span>Request updated:</span>
+        <span>{dmdTask?.["process"]?.["requestDate"]}</span>
+      </div>
+    </div>
+  </div>
+</div>
+
+<style>
+  h6 {
+    text-align: center;
+  }
+  .dmd-request-info {
+    width: fit-content;
+    display: inline-grid;
+    display: grid;
+    grid-template-areas: "a a";
+    gap: 2rem;
+    align-items: center;
+    margin-top: var(--perfect-fourth-4);
+  }
+</style>
