1dd49d26c0dec89d5ccb6e07a165d0378df67d30
Brittny Lapierre <brittny.lapierre@gmail.com>
2021-09-01 15:58:06 -0400

    Setting up update method

--- services/admin/src/lib/stores/dmdTasksStore.ts
+++ services/admin/src/lib/stores/dmdTasksStore.ts
@@ -1,4 +1,9 @@
-import type { DmdItemState, DmdTasksCache, DmdTaskState } from "$lib/types";
+import type {
+  DmdItemState,
+  DmdItemStates,
+  DmdTasksCache,
+  DmdTaskState,
+} from "$lib/types";
 import { get, writable } from "svelte/store";
 import type { TRPCClient } from "@trpc/client";
 import type {
@@ -34,6 +39,65 @@ function getTask(key: string): DmdTaskState {
   return dmdTasksCache[key];
 }
 
+function getTaskItemLength(
+  dmdTaskId: string,
+  items: DmdItemStates,
+  method: Function | undefined = undefined
+): number {
+  let numItems = 0;
+  for (var itemSlug in items) {
+    if (method !== undefined) method(dmdTaskId, itemSlug, items);
+    numItems++;
+  }
+  return numItems;
+}
+
+function evaluateLookupTaskItemResult(
+  dmdTaskId: string,
+  slug: string,
+  items: DmdItemStates,
+  result: any
+) {
+  const itemFound = result[1].found;
+  if (itemFound && result.length === 2 && "result" in result[1]) {
+    const itemData = result[1].result;
+    items[slug].noid = itemData.id;
+    items[slug].foundInAccess = "Yes";
+    updateTask(dmdTaskId, "itemStates", items);
+  } else {
+    items[slug].foundInAccess = "No";
+    updateTask(dmdTaskId, "itemStates", items);
+  }
+}
+
+async function lookupTaskItemsSubset(
+  prefix: string,
+  dmdTaskId: string,
+  items: DmdItemStates,
+  slugs: string[],
+  lapin: TRPCClient<LapinRouter>
+) {
+  const response = await lapin.query(
+    "slug.lookupMany",
+    slugs.map((slug) => `${prefix}.${slug}`)
+  );
+
+  let resultIndex = 0;
+  for (let slug of slugs) {
+    if (resultIndex < response.length) {
+      evaluateLookupTaskItemResult(
+        dmdTaskId,
+        slug,
+        items,
+        response[resultIndex]
+      );
+    } else {
+      throw "Not every item was searched.";
+    }
+    resultIndex++;
+  }
+}
+
 export const dmdTasksStore = {
   ...store,
   setTask,
@@ -45,70 +109,64 @@ export const dmdTasksStore = {
     prefix: string,
     lapin: TRPCClient<LapinRouter>
   ) => {
-    if (lapin) {
-      updateTask(dmdTaskId, "updateState", "ready");
-      updateTask(dmdTaskId, "lookupState", "loading");
-      let items = getTask(dmdTaskId).itemStates;
-      let index = 0;
-      let slugs: string[] = [];
+    updateTask(dmdTaskId, "updateState", "ready");
+    updateTask(dmdTaskId, "lookupState", "loading");
 
-      let numItems = 0;
-      for (var itemSlug in items) {
+    let items = getTask(dmdTaskId).itemStates;
+    let index = 0;
+    let slugs: string[] = [];
+
+    let numItems = getTaskItemLength(
+      dmdTaskId,
+      items,
+      (dmdTaskId, itemSlug, items) => {
         items[itemSlug].foundInAccess = "Searching...";
         updateTask(dmdTaskId, "itemStates", items);
-        numItems++;
       }
+    );
 
-      for (var itemSlug in items) {
-        slugs.push(itemSlug);
-        console.log(index, numItems, index === numItems - 1);
-        if ((index !== 0 && index % 100 === 0) || index === numItems - 1) {
-          const response = await lapin.query(
-            "slug.lookupMany",
-            slugs.map((slug) => `${prefix}.${slug}`)
-          );
-          let resultIndex = 0;
-          for (let slug of slugs) {
-            if (resultIndex < response.length) {
-              let result = response[resultIndex];
-              const itemFound = result[1].found;
-              if (itemFound && result.length === 2 && "result" in result[1]) {
-                const itemData = result[1].result;
-                items[slug].noid = itemData.id;
-                items[slug].foundInAccess = "Yes";
-                updateTask(dmdTaskId, "itemStates", items);
-              } else {
-                items[slug].foundInAccess = "No";
-                updateTask(dmdTaskId, "itemStates", items);
-              }
-            } else {
-              throw "Not every item was searched.";
-            }
-            resultIndex++;
-          }
-          slugs = [];
-        }
-        index++;
+    for (var itemSlug in items) {
+      slugs.push(itemSlug);
+      if ((index !== 0 && index % 100 === 0) || index === numItems - 1) {
+        await lookupTaskItemsSubset(prefix, dmdTaskId, items, slugs, lapin);
+        slugs = [];
       }
-      updateTask(dmdTaskId, "itemStates", items);
-      updateTask(dmdTaskId, "lookupState", "loaded");
-    } else console.log("No lapin router");
-    let dmdTasksCache: DmdTasksCache = get(store);
-    console.log("dmdTasksCache", dmdTasksCache);
+      index++;
+    }
+
+    updateTask(dmdTaskId, "itemStates", items);
+    updateTask(dmdTaskId, "lookupState", "loaded");
   },
   storeTaskItemsToSwift: async (
     dmdTaskId: string,
     lapin: TRPCClient<LapinRouter>
   ) => {
-    if (lapin) {
-      updateTask(dmdTaskId, "updateState", "updating");
-      let items = getTask(dmdTaskId).itemStates;
-      console.log("update", items);
-      for (var m in items) {
-        console.log(m);
+    updateTask(dmdTaskId, "updateState", "updating");
+
+    let items = getTask(dmdTaskId).itemStates;
+    let index = 0;
+    let itemSubset: DmdItemState[] = [];
+
+    let numItems = getTaskItemLength(
+      dmdTaskId,
+      items,
+      (dmdTaskId, itemSlug, items) => {
+        items[itemSlug].updatedInAccess = "Updating...";
+        updateTask(dmdTaskId, "itemStates", items);
       }
-      updateTask(dmdTaskId, "itemStates", items);
-      updateTask(dmdTaskId, "updateState", "updated");
-    } else console.log("No lapin router");
+    );
+
+    console.log("update", items);
+    for (var item in items) {
+      itemSubset.push(items[item]);
+      if ((index !== 0 && index % 100 === 0) || index === numItems - 1) {
+        //await store
+        itemSubset = [];
+      }
+      index++;
+    }
+
+    updateTask(dmdTaskId, "itemStates", items);
+    updateTask(dmdTaskId, "updateState", "updated");
   },
 };
