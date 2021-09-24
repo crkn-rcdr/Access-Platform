2b1eafca074e407ce764396c0caecfcef39b44ee
Brittny Lapierre <brittny.lapierre@gmail.com>
2021-09-24 11:01:32 -0400

    Set lookup to primary when clicked

--- services/admin/src/lib/components/dipstaging/DipstagingLookup.svelte
+++ services/admin/src/lib/components/dipstaging/DipstagingLookup.svelte
@@ -9,7 +9,10 @@
   /**
    *  @type { AccessPlatform } The access platform to look for the items in.
    */
-  let depositor: AccessPlatform;
+  let depositor: AccessPlatform = {
+    prefix: "none",
+    label: "",
+  };
   const BY_SLUG_LABEL = "Search by Slug";
   const BY_DATE_LABEL = "Search by Date";
   let lookupView: string = BY_SLUG_LABEL;
@@ -24,7 +27,8 @@
     lookupView = event.detail.option;
   }
 
-  function handleLookupPressedSlugList() {
+  function handleLookupPressedSlugList(event) {
+    event.stopPropagation();
     let slugList: string[] = slugListString.split(",");
 
     if (depositor.prefix !== "none")
@@ -38,10 +42,13 @@
         encodeURIComponent(encodeURIComponent(slug.trim()))
       );
     goto(`/smelter/keys/${slugList.toString()}`);
+    lookupDone = true;
   }
 
-  function handleLookupPressedDates() {
+  function handleLookupPressedDates(event) {
+    event.stopPropagation();
     goto(`/smelter/dates/${startDateStr},${endDateStr}`);
+    lookupDone = true;
   }
 
   function reset() {
@@ -76,10 +83,7 @@
           prefixedSlug = decodeURIComponent(decodeURIComponent(prefixedSlug));
           const arr = prefixedSlug.split(".");
           if (arr.length > 1) {
-            depositor = {
-              prefix: arr[0],
-              label: "",
-            };
+            depositor.prefix = arr[0];
             return arr.slice(1);
           } else return prefixedSlug;
         })
@@ -100,63 +104,73 @@
     else reset();
   }
 
+  function resetLookupColor() {
+    lookupDone = false;
+  }
+
   $: setDefaults($page.params);
 </script>
 
-<br />
-<ToggleButtons
-  activeIndex={lookupView === BY_SLUG_LABEL ? 0 : 1}
-  color={lookupDone ? "secondary" : "primary"}
-  options={[BY_SLUG_LABEL, BY_DATE_LABEL]}
-  on:select={changeView}
-/><br />
-<br />
+<div on:click={resetLookupColor}>
+  <br />
+  <ToggleButtons
+    activeIndex={lookupView === BY_SLUG_LABEL ? 0 : 1}
+    color={lookupDone ? "secondary" : "primary"}
+    options={[BY_SLUG_LABEL, BY_DATE_LABEL]}
+    on:select={changeView}
+  /><br />
+  <br />
 
-<div class="lookup-wrap auto-align">
-  <div class="user-input">
-    {#if lookupView === "Search by Slug"}
-      <div class="list-search">
-        <PrefixSelector bind:depositor />
-        <textarea
-          placeholder="Type a comma-seperated list of slugs"
-          bind:value={slugListString}
+  <div class="lookup-wrap auto-align">
+    <div class="user-input">
+      {#if lookupView === "Search by Slug"}
+        <div class="list-search">
+          <PrefixSelector bind:depositor />
+          <textarea
+            placeholder="Type a comma-seperated list of slugs"
+            bind:value={slugListString}
+          />
+        </div>
+      {:else}
+        <label for="start">Start date:</label>
+        <input
+          type="date"
+          id="start"
+          name="trip-start"
+          bind:value={startDateStr}
         />
-      </div>
-    {:else}
-      <label for="start">Start date:</label>
-      <input
-        type="date"
-        id="start"
-        name="trip-start"
-        bind:value={startDateStr}
-      />
 
-      <label for="end">End date:</label>
-      <input type="date" id="end" name="trip-end" bind:value={endDateStr} /><br
-      />
-    {/if}
+        <label for="end">End date:</label>
+        <input
+          type="date"
+          id="end"
+          name="trip-end"
+          bind:value={endDateStr}
+        /><br />
+      {/if}
+    </div>
   </div>
-</div>
 
-<br />
-<br />
-{#if lookupView === BY_SLUG_LABEL && slugListString?.length}
-  <button
-    class:primary={!lookupDone}
-    class:secondary={lookupDone}
-    on:click={handleLookupPressedSlugList}
-  >
-    {lookupDone ? "Lookup Again" : "Lookup"}
-  </button>
-{:else if lookupView === BY_DATE_LABEL && startDateStr?.length && endDateStr?.length}
-  <button
-    class:primary={!lookupDone}
-    class:secondary={lookupDone}
-    on:click={handleLookupPressedDates}
-  >
-    {lookupDone ? "Lookup Again" : "Lookup"}
-  </button>
-{/if}
+  <br />
+  <br />
+  {#if lookupView === BY_SLUG_LABEL && slugListString?.length}
+    <button
+      class:primary={!lookupDone}
+      class:secondary={lookupDone}
+      on:click={handleLookupPressedSlugList}
+    >
+      {lookupDone ? "Lookup Again" : "Lookup"}
+    </button>
+  {:else if lookupView === BY_DATE_LABEL && startDateStr?.length && endDateStr?.length}
+    <button
+      class:primary={!lookupDone}
+      class:secondary={lookupDone}
+      on:click={handleLookupPressedDates}
+    >
+      {lookupDone ? "Lookup Again" : "Lookup"}
+    </button>
+  {/if}
+</div>
 
 <style>
   .lookup-wrap {
