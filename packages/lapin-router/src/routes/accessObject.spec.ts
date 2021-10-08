d5955217f4ff9146e87010d82e6a090b743f4cd8
DivyaKrishnan26 <dkrishnan@crkn.ca>
2021-10-08 10:31:58 -0400

    Modifying the query path

--- packages/lapin-router/src/routes/accessObject.spec.ts
+++ packages/lapin-router/src/routes/accessObject.spec.ts
@@ -4,12 +4,18 @@ import anyTest, { TestInterface } from "ava";
 import { LapinRouter, router } from "../router.js";
 import { LapinContext } from "../context.js";
 
-
 //type LapinRouterContext = BaseContext & { lapin: LapinContext };
-const test = anyTest as TestInterface<{ lapin: LapinRouter, context: LapinContext}>;
-
+const test = anyTest as TestInterface<{
+  lapin: LapinRouter;
+  context: LapinContext;
+}>;
+const COLLECTION_SLUG = "oocihm.8_06941";
+const SLUG_SEARCH = "http://172.30.0.51:5858";
 test.serial("Lapin couch context reachable", async (t) => {
-  const result = await t.context.lapin.query({ "69429/s0vq2s46j98h" }); // this should take in the query path,
+  const result = await t.context.lapin.query({
+    path: SLUG_SEARCH,
+    procedure: COLLECTION_SLUG,
+  }); // this should take in the query path,
 
   t.deepEqual(result["resolved path"], { error: "Unresolved Path" });
-  })
+});
