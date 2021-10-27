badb8e53e0bb4448da6a2243bf7a66b2b5ef2ce2
Russell McOrmond <rmcormond@crkn.ca>
2021-10-27 10:55:34 -0400

    Count manifests which have PDF files (ocrPdf and born digital files) (#366)

---
+++ services/couchdb/access/design/tree/views/pdf.js
@@ -0,0 +1,20 @@
+module.exports = {
+  map: function (doc) {
+    if (doc.type === "manifest") {
+      if (typeof doc.file == "object") {
+        if (typeof doc.ocrPdf == "object") {
+          emit("both", doc.slug);
+        } else {
+          emit("file", doc.slug);
+        }
+      } else {
+        if (typeof doc.ocrPdf == "object") {
+          emit("ocrPdf", doc.slug);
+        } else {
+          emit("none", doc.slug);
+        }
+      }
+    }
+  },
+  reduce: "_count",
+};
