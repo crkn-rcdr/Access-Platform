226cbf1ad32a756c8a271e20d0ee6a63a5b98e84
Russell McOrmond <rmcormond@crkn.ca>
2021-10-28 13:18:10 -0400

    `master` isn't optional, but no need to check for it.

--- services/couchdb/canvas/design/access/views/cihmsourcepath.js
+++ services/couchdb/canvas/design/access/views/cihmsourcepath.js
@@ -1,10 +1,6 @@
 module.exports = {
   map: function (doc) {
-    if (
-      typeof doc.source == "object" &&
-      typeof doc.master == "object" &&
-      doc.source.from === "cihm"
-    ) {
+    if (typeof doc.source == "object" && doc.source.from === "cihm") {
       emit(doc.source.path, null);
     }
   },
