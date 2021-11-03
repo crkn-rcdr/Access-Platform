9030198ee04bb23449074cf32bfc103e6f39cfc7
Russell McOrmond <rmcormond@crkn.ca>
2021-11-03 17:34:24 -0400

    Updates...

---
+++ services/couchdb/dipstaging/design/access/updates/cancelOCR.js
@@ -0,0 +1,27 @@
+module.exports = function (doc, req) {
+  const {
+    successReturn,
+    errorReturn,
+    extractJSONFromBody,
+    timestamp,
+  } = require("views/lib/prelude");
+
+  if (!doc) {
+    return errorReturn(`No document found with id ${req.id}`, 404);
+  }
+
+  const data = extractJSONFromBody(req);
+  if (!data) {
+    return errorReturn(`Could not parse request body as JSON: ${req.body}`);
+  }
+
+  const now = timestamp();
+
+  const { user } = data;
+
+  doc.user = user;
+  doc.updated = now;
+  delete doc.smelt;
+
+  return successReturn(doc, "ok");
+};
---
+++ services/couchdb/dipstaging/design/access/updates/requestOCR.js
@@ -0,0 +1,28 @@
+module.exports = function (doc, req) {
+  const {
+    successReturn,
+    errorReturn,
+    extractJSONFromBody,
+    timestamp,
+  } = require("views/lib/prelude");
+
+  if (!doc) {
+    return errorReturn(`No document found with id ${req.id}`, 404);
+  }
+
+  const data = extractJSONFromBody(req);
+  if (!data) {
+    return errorReturn(`Could not parse request body as JSON: ${req.body}`);
+  }
+
+  const now = timestamp();
+
+  const { user, slug } = data;
+
+  doc.user = user;
+  doc.slug = slug;
+  doc.updated = now;
+  doc.smelt = { requestDate: now };
+
+  return successReturn(doc, "ok");
+};
