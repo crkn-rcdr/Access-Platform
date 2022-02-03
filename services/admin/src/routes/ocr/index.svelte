0f72ae7622fa7e0972778cc942a3de50866aedea
Brittny Lapierre <brittny.lapierre@gmail.com>
2022-02-03 12:02:33 -0500

    setup

---
+++ services/admin/src/routes/ocr/index.svelte
@@ -0,0 +1,15 @@
+<script context="module" lang="ts">
+</script>
+
+<script lang="ts">
+  // script
+</script>
+
+<header>title</header>
+<button>new</button>
+<table>
+  export queue<br />
+  exported<br />
+  import queue<br />
+  imported<br />
+</table>
---
+++ services/admin/src/routes/ocr/new/index.svelte
@@ -0,0 +1,26 @@
+<script context="module" lang="ts">
+</script>
+
+<script lang="ts">
+  import PrefixSlugSearchBox from "$lib/components/access-objects/PrefixSlugSearchBox.svelte";
+</script>
+
+<label>
+  Batch Name:
+  <input />
+</label>
+
+<label>Search for canvases</label>
+<PrefixSlugSearchBox />
+
+<table>
+  Select<br />
+  Slug<br />
+  Label<br />
+  # Canvases<br />
+  Error ---
+</table>
+
+total # selected Canvases
+<cancel button />
+<create button />
