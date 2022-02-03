1554fb1e0d37b49dfc93aacbdee114c37ddfdb55
Brittny Lapierre <brittny.lapierre@gmail.com>
2022-02-03 14:37:01 -0500

    Page layouts

--- services/admin/src/routes/ocr/index.svelte
+++ services/admin/src/routes/ocr/index.svelte
@@ -5,11 +5,44 @@
   // script
 </script>
 
-<header>title</header>
-<button>new</button>
-<table>
-  export queue<br />
-  exported<br />
-  import queue<br />
-  imported<br />
-</table>
+<br />
+<br />
+<div class="wrapper">
+  <div class="title auto-align auto-align__a-center">
+    <h6>OCR Batches</h6>
+    <button class="create-button primary">Create New OCR Batch</button>
+  </div>
+  <br />
+  <br />
+
+  <!--
+    export queue<br />
+    exported<br />
+    import queue<br />
+    imported<br />-->
+  <table>
+    <thead>
+      <th> Name </th>
+      <th> Status </th>
+    </thead>
+    <tbody>
+      <tr>
+        <td> onetwothree </td>
+        <td> Success </td>
+      </tr>
+    </tbody>
+  </table>
+</div>
+
+<style>
+  .title {
+    width: 100%;
+  }
+  h6 {
+    flex: 9;
+    margin: 0 !important;
+  }
+  .create-button {
+    flex: 2;
+  }
+</style>
