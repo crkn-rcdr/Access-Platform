82ec8469ba2124c026b6be06a9de20f718a5a045
Russell McOrmond <rmcormond@crkn.ca>
2023-02-10 16:35:30 -0500

    Different statistics -- to see progress during generation of new multi-page ODR PDFs.

--- services/couchdb/access/design/tree/views/pdf.js
+++ services/couchdb/access/design/tree/views/pdf.js
@@ -1,20 +1,33 @@
 module.exports = {
   map: function (doc) {
-    if (doc.type !== "collection") {
-      if (typeof doc.file == "object") {
-        if (typeof doc.ocrPdf == "object") {
-          emit("both", doc.slug);
-        } else {
-          emit("file", doc.slug);
-        }
+    var type = "none";
+    if ("type" in doc) {
+      type = doc.type;
+    }
+
+    var hasFile = "none";
+    if (typeof doc.file == "object") {
+      if ("extension" in doc.file) {
+        hasFile = doc.file.extension;
+      } else if ("path" in doc.file) {
+        hasFile = "path";
       } else {
-        if (typeof doc.ocrPdf == "object") {
-          emit("ocrPdf", doc.slug);
-        } else {
-          emit("none", doc.slug);
-        }
+        hasFile = "oops";
       }
     }
+
+    var hasOcrPdf = "none";
+    if (typeof doc.ocrPdf == "object") {
+      if ("extension" in doc.ocrPdf) {
+        hasOcrPdf = doc.ocrPdf.extension;
+      } else if ("path" in doc.ocrPdf) {
+        hasOcrPdf = "path";
+      } else {
+        hasOcrPdf = "oops";
+      }
+    }
+
+    emit([type, hasOcrPdf, hasFile], doc.slug);
   },
   reduce: "_count",
 };
