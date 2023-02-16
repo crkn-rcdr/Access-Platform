eb705ccc8f0bbab665f7da74d8e7340909685818
Russell McOrmond <rmcormond@crkn.ca>
2023-02-16 06:15:48 -0500

    CouchDB design for simple service to generate multi-page PDFs. (#576)
    * Simple views and updates for https://github.com/crkn-rcdr/Access-Platform/issues/90
    
    * Missed the important setting of the ocrPdf FileRef.
    
    * Null is also an object -- learning Javascript...
    
    * Different statistics -- to see progress during generation of new multi-page ODR PDFs.
    
    * Also need canvas count numbers to make sense of table.

--- services/couchdb/access/design/tree/views/pdf.js
+++ services/couchdb/access/design/tree/views/pdf.js
@@ -1,20 +1,44 @@
 module.exports = {
   map: function (doc) {
-    if (doc.type !== "collection") {
-      if (typeof doc.file == "object") {
-        if (typeof doc.ocrPdf == "object") {
-          emit("both", doc.slug);
-        } else {
-          emit("file", doc.slug);
-        }
+    var type = "none";
+    if ("type" in doc) {
+      type = doc.type;
+    }
+
+    var hasFile = "none";
+    if (typeof doc.file == "object") {
+      if ("extension" in doc.file) {
+        hasFile = doc.file.extension;
+      } else if ("path" in doc.file) {
+        hasFile = "path";
       } else {
-        if (typeof doc.ocrPdf == "object") {
-          emit("ocrPdf", doc.slug);
-        } else {
-          emit("none", doc.slug);
-        }
+        hasFile = "oops";
       }
     }
+
+    var hasOcrPdf = "none";
+    if (typeof doc.ocrPdf == "object") {
+      if ("extension" in doc.ocrPdf) {
+        hasOcrPdf = doc.ocrPdf.extension;
+      } else if ("path" in doc.ocrPdf) {
+        hasOcrPdf = "path";
+      } else {
+        hasOcrPdf = "oops";
+      }
+    }
+
+    var hasCanvases = "none";
+    if (Array.isArray(doc.canvases)) {
+      if (doc.canvases.length == 0) {
+        hasCanvases = "0";
+      } else if (doc.canvases.length == 1) {
+        hasCanvases = "1";
+      } else {
+        hasCanvases = "2+";
+      }
+    }
+
+    emit([type, hasOcrPdf, hasFile, hasCanvases], doc.slug);
   },
   reduce: "_count",
 };
